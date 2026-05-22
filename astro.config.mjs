b4cffc5cd67f03fd22c9ad91d268b2455cd9a3b2
nutzlichsein <nutzlichsein+github@gmail.com>
2026-05-22 14:26:56 +0800

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://suryowibowo.github.io',
-  // base: '/2026-web-astro', // Uncomment for GitHub Pages deployment
+  site: 'https://suryo.terangtanah.com',
 });
